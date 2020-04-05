a5bc389df574407aa773c5007b1303500aaa6e54
Facu <subiabrewd@gmail.com>
2020-04-05 11:39:15 +0200

    Fix bug with attack axis representation

--- public/socket.js
+++ public/socket.js
@@ -29,19 +29,19 @@ socket.on('attack', (player) => {
     if (player.axis == 'x') {
         box.setAttribute(
             'style',
-            `width: ${100}vw;
-             height: ${player.size}vw;
-             top: ${player.position.y}vh;
-             left: ${0}vw;
+            `width: ${player.size}vw;
+             height: ${100}vh;
+             top: ${0}vh;
+             left: ${player.position.x}vw;
              box-shadow: 0 0 22px ${player.color}`
         )
     } else {
         box.setAttribute(
             'style',
-            `width: ${player.size}vw;
-             height: ${100}vh;
-             top: ${0}vh;
-             left: ${player.position.x}vw;
+            `width: ${100}vw;
+             height: ${player.size}vw;
+             top: ${player.position.y}vh;
+             left: ${0}vw;
              box-shadow: 0 0 22px ${player.color}`
         )
     }
