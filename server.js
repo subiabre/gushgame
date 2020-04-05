d67b968d9e753378426dcff14473b1779acfc45a
Facu <subiabrewd@gmail.com>
2020-04-05 07:36:25 +0200

    Update player position

--- server.js
+++ server.js
@@ -33,6 +33,19 @@ app.get('/player/:id', (req, res) => {
     res.send(player)
 })
 
+app.post('/player/:id', (req, res) => {
+    let player = game.players.getPlayer(req.params.id)
+
+    if (!player) res.send(false)
+
+    player.setBoard(req.body.board.width, req.body.board.height)
+    player.setPosition(req.body.position.x, req.body.position.y)
+
+    game.players.list[player.id] = player
+
+    res.send(player)
+}) 
+
 var listener = http.listen(process.env.PORT || 4000, () => {
     console.log(`Server listening at http://localhost:${listener.address().port}`)
 })
