0db9edc8ed5c98125a9773ed796359eecd372e41
Facu <subiabrewd@gmail.com>
2020-04-05 06:06:39 +0200

    Add game class

---
+++ src/game.js
@@ -0,0 +1,45 @@
+"use strict";
+
+class PlayerList
+{
+    constructor()
+    {
+        this.list = {}
+    }
+            
+    addPlayer(player)
+    {
+        this.list[player.id] = player
+    }
+
+    getPlayer(id)
+    {
+        return this.list[id]
+    }
+
+    removePlayer(player)
+    {
+        delete this.list[player.id]
+    }
+
+    countPlayers()
+    {
+        let players = Object.keys(this.list).length
+        
+        return players
+    }
+}
+
+class Game
+{
+    constructor()
+    {
+        this.players = new PlayerList
+
+        this.playersOnAttack = new PlayerList
+
+        this.playersOnDead = new PlayerList
+    }
+}
+
+module.exports = Game
